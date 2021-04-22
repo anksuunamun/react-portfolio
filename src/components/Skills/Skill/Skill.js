e0c9c0ab3ee3f7c8564d08e567ea89ec856cce4e
anksuunamun <shishkina_veronika_10@mail.ru>
2021-04-22 13:22:14 +0300

    styles: fix styles for skills block

--- src/components/Skills/Skill/Skill.js
+++ src/components/Skills/Skill/Skill.js
@@ -2,14 +2,15 @@ import React from 'react';
 import styles from './Skill.module.scss';
 
 
+
 const Skill = (props) => {
     return (
         <div className={`${styles.skillWrapper}`}>
-            <div className={styles.skillImg}>
-                <img src={props.skillIcon} alt=""/>
+            <img src={props.skillIcon} alt=""/>
+            <div className={styles.skill}>
+                <h4>{props.skillName}</h4>
+                <div className={styles.skillDescription}>{props.description}</div>
             </div>
-            <div className={styles.skillName}>{props.skillName}</div>
-            <div>{props.description}</div>
         </div>
     )
 }
--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import styles from './Skills.module.scss';
 import Skill from "./Skill/Skill";
-import reduxLogo from '../../assets/images/redux-cover-imgage-1024x768.jpg';
-import reactLogo from '../../assets/images/react-logo.png';
-import typescript from '../../assets/images/1_TpbxEQy4ckB-g31PwUQPlg.png';
+import reduxLogo from '../../assets/images/reduxLogo.png';
+import typescript from '../../assets/images/tsLogo.webp';
+import reactLogo from '../../assets/images/reactLogo.svg';
+import jsLogo from '../../assets/images/javascript.svg';
+import storyLogo from '../../assets/images/storybook_logo_icon_144839.png';
+import axiosLogo from '../../assets/images/ClipartKey_1582958.png';
 import Title from "../../common/components/Title/Title";
+
 
 const Skills = (props) => {
     const skills = [
@@ -21,6 +25,21 @@ const Skills = (props) => {
             icon: typescript,
             description: 'TypeScript extends JavaScript by adding types.',
         },
+        {
+            name: 'Javascript',
+            icon: jsLogo,
+            description: 'JS is a programming language that conforms to the ECMAScript specification.'
+        },
+        {
+            name: 'Storybook',
+            icon: storyLogo,
+            description: 'Storybook is an open source tool for developing UI components in isolation for React, Vue, Angular, and more.'
+        },
+        {
+            name: 'Axios',
+            icon: axiosLogo,
+            description: 'Axios is a promise based HTTP client for the browser and Node.js.'
+        }
     ]
 
     const skillItems = skills.map(skill => <Skill skillIcon={skill.icon}
@@ -31,7 +50,10 @@ const Skills = (props) => {
     return (
         <div className={`${styles.skillsWrapper}`}>
             <div className={`contentWrapper ${styles.skillBlock}`}>
-                <Title title={'My Skills'}/>
+                <div className={styles.headersWrapper}>
+                    <h6>libraries, programming languages & tools</h6>
+                    <Title title={'My Skills'}/>
+                </div>
                 <div className={styles.skills}>{skillItems}</div>
             </div>
         </div>
